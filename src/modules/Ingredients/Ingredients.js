89335af785818fd3817c2d5dabbc0183dfa48f2b
mfscvn <scavone.mf@gmail.com>
2020-11-11 11:20:28 -0300

    wip

--- src/modules/Ingredients/Ingredients.js
+++ src/modules/Ingredients/Ingredients.js
@@ -1,5 +1,7 @@
 import React, { Fragment, useState } from 'react';
 
+import PropTypes from 'prop-types';
+
 import styles from './Ingredients.module.scss';
 import IngredientModal from '../../components/IngredientsModal';
 
