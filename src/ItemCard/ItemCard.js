a3b870a408974cf148aad53fcdde354cef6c099b
Pablo Archenti <pabloarchenti@cookunity.com>
2020-11-26 17:54:41 -0300

    Delete ItemCard example

--- src/ItemCard/ItemCard.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import styles from './ItemCard.module.scss'
-
-const ItemCard = args => {
-  return <div className={styles.item}>Example Card</div>
-}
-
-export default ItemCard
