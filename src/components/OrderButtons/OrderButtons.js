cbd99439fc0b65a9eb19ed3f0ea1299639f2ea14
Pablo Archenti <pabloarchenti@cookunity.com>
2021-04-13 22:25:34 -0300

    Complete OrderButton (without test)

--- src/components/OrderButtons/OrderButtons.js
+++ src/components/OrderButtons/OrderButtons.js
@@ -4,65 +4,54 @@ import PropTypes from 'prop-types'
 import Button from '../../common/Buttons'
 import styles from './OrderButtons.module.scss'
 
-const OrderButtons = ({
-  qtyOrdered,
-  qtyTotal,
-  mealDetail,
-  removeProduct,
-  addProduct
-}) => {
-  const minusEnabled = qtyOrdered > 0
-  const plusEnabled = qtyOrdered < qtyTotal && qtyTotal !== 0
+const OrderButtons = ({ordered, total, onRemove, onAdd}) => {
+  const minusEnabled = ordered > 0
+  const plusEnabled = ordered < total
 
   return (
-    <section className={styles.mealFooter}>
-      {qtyOrdered > 0 ? (
+    <section className={styles.orderButton}>
+      {ordered > 0 ? (
         <div className={styles.buttonsContainer}>
           <div className={styles.buttonContainer}>
             <button
               className={`${styles.mealButton} ${styles.minus}`}
-              onClick={() => minusEnabled && removeProduct(mealDetail)}
+              onClick={() => minusEnabled && onRemove()}
             />
           </div>
 
-          <div className={styles.orderContainer}>{qtyOrdered}</div>
+          <div className={styles.orderContainer}>{ordered}</div>
 
           <div className={styles.buttonContainer}>
             <button
               className={`${styles.mealButton} ${styles.plus}`}
-              onClick={() => plusEnabled && addProduct(mealDetail)}
+              onClick={() => plusEnabled && onAdd()}
               disabled={!plusEnabled}
             />
           </div>
         </div>
       ) : (
-        <Button
-          dark
-          onClick={() =>
-            (qtyOrdered < qtyTotal || qtyTotal !== 0) && addProduct(mealDetail)
-          }
-        >
-          <div>Add meal</div>
-        </Button>
+        plusEnabled && (
+          <Button dark onClick={() => onAdd()}>
+            Add meal
+          </Button>
+        )
       )}
     </section>
   )
 }
 
 OrderButtons.propTypes = {
-  qtyOrdered: PropTypes.number,
-  qtyTotal: PropTypes.number,
-  mealDetail: PropTypes.object.isRequired,
-  removeProduct: PropTypes.func,
-  addProduct: PropTypes.func
+  ordered: PropTypes.number,
+  total: PropTypes.number,
+  onRemove: PropTypes.func,
+  onAdd: PropTypes.func
 }
 
 OrderButtons.defaultProps = {
-  qtyOrdered: 0,
-  qtyTotal: 0,
-  mealDetail: {},
-  removeProduct: () => {},
-  addProduct: () => {}
+  ordered: 0,
+  total: 0,
+  onRemove: () => {},
+  onAdd: () => {}
 }
 
 export default OrderButtons
--- src/components/OrderButtons/OrderButtons.stories.js
+++ src/components/OrderButtons/OrderButtons.stories.js
@@ -1,8 +1,6 @@
 import React from 'react'
 import OrderButtons from './OrderButtons'
 
-import {meal as mealDetail} from './__mock__.json'
-
 export default {
   title: 'OrderButtons',
   component: OrderButtons
@@ -11,29 +9,32 @@ export default {
 const Template = args => <OrderButtons {...args} />
 
 export const Add = Template.bind({})
+Add.args = {
+  total: 2
+}
 
 export const Added = Template.bind({})
 Added.args = {
-  qtyOrdered: 2,
-  mealDetail,
-  removeProduct: () => {
-    console.log('removeProduct')
+  ordered: 2,
+  total: 3,
+  onRemove: () => {
+    console.log('onRemove()')
   },
-  addProduct: () => {
-    console.log('addProduct')
-  },
-  qtyTotal: 10
+  onAdd: () => {
+    console.log('onAdd()')
+  }
 }
 
 export const NoMore = Template.bind({})
 NoMore.args = {
-  qtyOrdered: 2,
-  mealDetail,
-  removeProduct: () => {
-    console.log('removeProduct')
+  ordered: 2,
+  total: 2,
+  onRemove: () => {
+    console.log('onRemove()')
   },
-  addProduct: () => {
-    console.log('addProduct')
-  },
-  qtyTotal: 2
+  onAdd: () => {
+    console.log('onAdd()')
+  }
 }
+
+export const Empty = Template.bind({})
