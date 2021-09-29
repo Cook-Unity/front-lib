1219a84998fb37b72764c2e455354ed49f725123
Nicolas Lobruno <nicolaslobruno@Nicolass-MacBook-Air.local>
2021-09-29 17:28:35 -0300

    Correction test

--- src/components/CollectionMeals/components/CollectionMealHeader/CollectionMealHeader.test.js
+++ src/components/CollectionMeals/components/CollectionMealHeader/CollectionMealHeader.test.js
@@ -1,6 +1,5 @@
 import {render, screen} from '@testing-library/react'
 import React from 'react'
-import userEvent from '@testing-library/user-event'
 
 import CollectionMealIcon from './index'
 
@@ -8,8 +7,6 @@ const IMAGE =
   'https://d1ureao5dcqpo4.cloudfront.net/media/merchandiseSets/chef-akhtar-chicken-tinga-tacos.jpg'
 
 describe('CollectionMealIcon', () => {
-  const handleOnClickMock = callback => callback()
-
   it('Required props', () => {
     render(<CollectionMealIcon title="From the Sea" image={IMAGE} />)
     expect(screen.getByText('From')).toBeVisible()
@@ -26,20 +23,5 @@ describe('CollectionMealIcon', () => {
       />
     )
     expect(screen.getByText('UPDATED WEEKLY')).toBeVisible()
-  })
-
-  it('Click in the view all', () => {
-    const clickMock = jest.fn()
-    render(
-      <CollectionMealIcon
-        title="From the Sea"
-        image={IMAGE}
-        backgroundColor="#F6E1D6"
-        updatedWeekly
-        handleOnClick={() => handleOnClickMock(clickMock)}
-      />
-    )
-    userEvent.click(screen.getByTestId('view-all'))
-    expect(clickMock).toHaveBeenCalled()
   })
 })
