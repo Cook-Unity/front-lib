ce4788486d50d7a509bc54435b25114a5f5e7260
Carolina Spina <carolina.spina@cookunity.com>
2022-11-09 14:13:17 +0000

    feat: new category cover type, breaker component

---
+++ src/components/Breaker/__mock__.js
@@ -0,0 +1,6 @@
+export const breaker_basic = {
+  title: 'How to make the best of your meals...',
+  description:
+    'One of the most pleasing aspects of eating a delicious meal are the varying textures of the dish. If your dinner consists of creamy mashed potatoes or pan-seared chicken breast, a thin sauce like [Chef’s Sauce Name] is perfect for tying both components together and making the chicken juicier.',
+  imageUrl: 'https://d1ureao5dcqpo4.cloudfront.net/media/cover/sides-cover.jpg'
+}
---
+++ src/components/Breaker/styled.jsx
@@ -0,0 +1,46 @@
+import styled from 'styled-components'
+import {device} from '../../utils/mediaQueries'
+
+export const Wrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  padding: 16px;
+  border-top: 1px solid #dddddd;
+  border-bottom: 1px solid #dddddd;
+  @media ${device.tablet} {
+    flex-direction: column;
+  }
+`
+
+export const ImageSection = styled.img`
+  max-width: 270px;
+  height: 150px;
+  border-radius: 4px;
+  margin-right: 24px;
+  @media ${device.tablet} {
+    margin-right: 0;
+    margin-bottom: 13px;
+    min-width: 100%;
+  }
+`
+
+export const TextSection = styled.div`
+  display: flex;
+  flex-direction: column;
+  color: #000000;
+  justify-content: center;
+`
+
+export const Title = styled.div`
+  font-family: Gilroy Semibold;
+  font-size: 20px;
+  line-height: 28px;
+  margin-bottom: 8px;
+`
+
+export const Subtitle = styled.div`
+  font-family: Roboto;
+  font-size: 12px;
+  font-weight: 400;
+  line-height: 20px;
+`
