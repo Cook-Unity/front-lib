7b87a596dcaa08adcb768833dc0aedd10b0eff1d
Cristian Martinez <cristian@cookunity.com>
2022-11-22 00:27:10 -0300

    feat(rebranding): adding new input component for rebranding

---
+++ src/cui/components/CuiInput/CuiInput.jsx
@@ -0,0 +1,30 @@
+import classNames from 'classnames'
+import React from 'react'
+import './CuiInput.scss'
+
+const CuiInput = ({
+  fill = 'solid',
+  loading = false,
+  children,
+  className,
+  color,
+  size,
+  ...props
+}) => {
+  return (
+    <input
+      type="text"
+      className={classNames(
+        'cui-input',
+        `cui-input-${color}`,
+        `cui-input-${size}`,
+        `cui-input-${color}-${fill}`,
+        loading && `cui-input-${color}-${fill}-loading`,
+        className
+      )}
+      {...props}
+    />
+  )
+}
+
+export default CuiInput
