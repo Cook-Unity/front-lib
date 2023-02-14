50a86f222763076b5594771c48d77d7edb04adec
Carolina Spina <carolina.spina@cookunity.com>
2023-02-14 17:38:33 -0300

    fix: filters issue

--- src/cui/components/CuiMenuByBar/CuiMenuByBar.jsx
+++ src/cui/components/CuiMenuByBar/CuiMenuByBar.jsx
@@ -43,7 +43,8 @@ const CuiMenuByBar = ({
     }
   }
 
-  const handleMenuClick = () => {
+  const handleMenuClick = e => {
+    e.stopPropagation()
     setMenuOpen(!isMenuOpen)
     onMenuByClick(!isMenuOpen)
   }
