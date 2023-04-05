b2fc0b9ec4c40983f18a0487963d18239e4c75c8
David Rearte <david.rearte@cookunity.com>
2023-04-05 00:20:10 -0300

    feat(bottom-sheet): added bottom sheet

---
+++ src/cui/components/CuiBottomSheet/CuiBottomSheet.jsx
@@ -0,0 +1,67 @@
+import React, {useLayoutEffect, useState} from 'react'
+import CuiIcon from '../CuiIcon/CuiIcon'
+import './CuiBottomSheet.scss'
+import {createPortal} from 'react-dom'
+import classnames from 'classnames'
+
+const Header = ({onRequestClose, children, className}) => {
+  return (
+    <div className={classnames('cui-bottom-sheet__header', className)}>
+      <div className="cui-bottom-sheet__header__title">{children}</div>
+      <button
+        className="cui-bottom-sheet__header__close-button"
+        onClick={onRequestClose}
+      >
+        <CuiIcon
+          name="close"
+          className="cui-bottom-sheet__header__close-button__close-icon"
+          role="icon"
+        />
+      </button>
+    </div>
+  )
+}
+
+const Body = ({children}) => {
+  return <div className="cui-bottom-sheet__sheet__body">{children}</div>
+}
+
+// Click outside if required https://stackoverflow.com/questions/32553158/detect-click-outside-react-component
+const BottomSheet = ({children, className = ''}) => {
+  return (
+    <div className={classnames('cui-bottom-sheet', className)}>
+      <div className="cui-bottom-sheet__overlay" />
+      <div className="cui-bottom-sheet__sheet">{children}</div>
+    </div>
+  )
+}
+
+export const CuiBottomSheet = ({isOpen, ...props}) => {
+  const [delayedIsOpen, setIsOpen] = useState(isOpen)
+
+  useLayoutEffect(() => {
+    let timeout
+    if (isOpen) {
+      setIsOpen(isOpen)
+      timeout = setTimeout(() => {
+        document.body.classList.add('cui-bottom-sheet--open')
+      }, 0)
+    } else {
+      document.body.classList.remove('cui-bottom-sheet--open')
+      timeout = setTimeout(() => {
+        setIsOpen(isOpen)
+      }, 500)
+    }
+    return () => {
+      document.body.classList.remove('cui-bottom-sheet--open')
+      clearTimeout(timeout)
+    }
+  }, [isOpen])
+
+  return (
+    delayedIsOpen && createPortal(<BottomSheet {...props} />, document.body)
+  )
+}
+
+CuiBottomSheet.Header = Header
+CuiBottomSheet.Body = Body
---
+++ src/cui/components/CuiBottomSheet/CuiBottomSheet.stories.js
@@ -0,0 +1,121 @@
+import React, {useState} from 'react'
+import classnames from 'classnames'
+import {CuiBottomSheet} from './CuiBottomSheet'
+import './CuiBottomSheet.stories.scss'
+
+export default {
+  title: 'Cui / Components / CuiBottomSheet',
+  component: CuiBottomSheet,
+  argTypes: {
+    src: {
+      control: 'text',
+      defaultValue:
+        'https://static.cookunity.com/cross/front-lib/images/cui-demo-banner-image.jpeg'
+    },
+    title: {
+      control: 'text',
+      defaultValue: 'Recommendations'
+    },
+    withBackgroundImage: {
+      control: 'boolean',
+      defaultValue: false
+    }
+  }
+}
+
+const Template = ({withBackgroundImage, ...props}) => {
+  const [isOpen, setIsOpen] = useState(false)
+
+  return (
+    <div>
+      <p>
+        Lorem Ipsum is simply dummy text of the printing and typesetting
+        industry. Lorem Ipsum has been the industry's standard dummy text ever
+        since the 1500s, when an unknown printer took a galley of type and
+        scrambled it to make a type specimen book. It has survived not only five
+        centuries, but also the leap into electronic typesetting, remaining
+        essentially unchanged. It was popularised in the 1960s with the release
+        of Letraset sheets containing Lorem Ipsum passages, and more recently
+        with desktop publishing software like Aldus PageMaker including versions
+        of Lorem Ipsum.
+      </p>
+      <p>
+        Lorem Ipsum is simply dummy text of the printing and typesetting
+        industry. Lorem Ipsum has been the industry's standard dummy text ever
+        since the 1500s, when an unknown printer took a galley of type and
+        scrambled it to make a type specimen book. It has survived not only five
+        centuries, but also the leap into electronic typesetting, remaining
+        essentially unchanged. It was popularised in the 1960s with the release
+        of Letraset sheets containing Lorem Ipsum passages, and more recently
+        with desktop publishing software like Aldus PageMaker including versions
+        of Lorem Ipsum.
+      </p>
+      <button onClick={() => setIsOpen(prev => !prev)}>OPEN/CLOSE</button>
+      <CuiBottomSheet
+        {...props}
+        isOpen={isOpen}
+        className={classnames({'with-background-image': withBackgroundImage})}
+      >
+        <CuiBottomSheet.Header onRequestClose={() => setIsOpen(false)}>
+          Recommendations
+        </CuiBottomSheet.Header>
+        <CuiBottomSheet.Body>
+          <p>START</p>
+          <p>
+            lorem ipsum dolor sit amet, consectetur adipis lorem ipsum dolor sit
+            amet, consectetur adipis lorem ipsum dolor sit amet, consectetur
+            adipis lorem ipsum dolor sit amet, consectetur adipis
+          </p>
+          <p>
+            lorem ipsum dolor sit amet, consectetur adipis lorem ipsum dolor sit
+            amet, consectetur adipis lorem ipsum dolor sit amet, consectetur
+            adipis lorem ipsum dolor sit amet, consectetur adipis
+          </p>
+          <p>
+            lorem ipsum dolor sit amet, consectetur adipis lorem ipsum dolor sit
+            amet, consectetur adipis lorem ipsum dolor sit amet, consectetur
+            adipis lorem ipsum dolor sit amet, consectetur adipis
+          </p>
+          <p>
+            lorem ipsum dolor sit amet, consectetur adipis lorem ipsum dolor sit
+            amet, consectetur adipis lorem ipsum dolor sit amet, consectetur
+            adipis lorem ipsum dolor sit amet, consectetur adipis
+          </p>
+          <p>
+            lorem ipsum dolor sit amet, consectetur adipis lorem ipsum dolor sit
+            amet, consectetur adipis lorem ipsum dolor sit amet, consectetur
+            adipis lorem ipsum dolor sit amet, consectetur adipis
+          </p>
+          <p>
+            lorem ipsum dolor sit amet, consectetur adipis lorem ipsum dolor sit
+            amet, consectetur adipis lorem ipsum dolor sit amet, consectetur
+            adipis lorem ipsum dolor sit amet, consectetur adipis
+          </p>
+          <p>
+            lorem ipsum dolor sit amet, consectetur adipis lorem ipsum dolor sit
+            amet, consectetur adipis lorem ipsum dolor sit amet, consectetur
+            adipis lorem ipsum dolor sit amet, consectetur adipis
+          </p>
+          <p>
+            lorem ipsum dolor sit amet, consectetur adipis lorem ipsum dolor sit
+            amet, consectetur adipis lorem ipsum dolor sit amet, consectetur
+            adipis lorem ipsum dolor sit amet, consectetur adipis
+          </p>
+          <p>
+            lorem ipsum dolor sit amet, consectetur adipis lorem ipsum dolor sit
+            amet, consectetur adipis lorem ipsum dolor sit amet, consectetur
+            adipis lorem ipsum dolor sit amet, consectetur adipis
+          </p>
+          <p>
+            lorem ipsum dolor sit amet, consectetur adipis lorem ipsum dolor sit
+            amet, consectetur adipis lorem ipsum dolor sit amet, consectetur
+            adipis lorem ipsum dolor sit amet, consectetur adipis
+          </p>
+          <p>END</p>
+        </CuiBottomSheet.Body>
+      </CuiBottomSheet>
+    </div>
+  )
+}
+
+export const Default = Template.bind({})
