b12293c1d2f8d4edb34e676a262e0761a3ea734a
Juan Pablo Pardal <juan.pardal@cookunity.com>
2023-10-17 13:04:16 -0300

    add strikethrough price

--- src/cui/modules/CuiMealCard/components/CuiMealActions/CuiMealActions.stories.js
+++ src/cui/modules/CuiMealCard/components/CuiMealActions/CuiMealActions.stories.js
@@ -6,6 +6,12 @@ export default {
   title: 'Cui / Modules / CuiMealCard / Components / CuiMealActions',
   component: CuiMealActions,
   argTypes: {
+    priceText: {
+      defaultValue: 'Add extra for $19.99'
+    },
+    strikethroughPrice: {
+      defaultValue: '$30.99'
+    },
     tooltipText: {
       defaultValue: 'ASD'
     }
