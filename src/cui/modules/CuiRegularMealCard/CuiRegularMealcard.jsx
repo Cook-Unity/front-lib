1723b49758806c98b01a8cb78b4f4a4cd5773600
Carolina Spina <carolina.spina@cookunity.com>
2023-10-23 13:12:20 -0300

    fix: added missing imports

--- src/cui/modules/CuiRegularMealCard/CuiRegularMealcard.jsx
+++ src/cui/modules/CuiRegularMealCard/CuiRegularMealcard.jsx
@@ -1,3 +1,4 @@
+import React from 'react'
 import './CuiRegularMealcard.scss'
 import CuiTag from '../../components/CuiTag/CuiTag'
 import CuiMealFavorite from '../CuiMealCard/components/CuiMealFavorite/CuiMealFavorite'
@@ -7,43 +8,42 @@ import CuiMealActions from '../CuiMealCard/components/CuiMealActions/CuiMealActi
 import CuiMealSummary from '../CuiMealCard/components/CuiMealSummary/CuiMealSummary'
 import classnames from 'classnames'
 
-
-
 const CuiRegularMealCard = ({
-    meal = {},
-    mealDetailUrl,
-    outOfStock,
-    onMealClick,
-    onChefClick,
-    chef,
-    mainTags = [],
-    onLikeMeal,
-    onUnlikeMeal,
-    isLiked,
-    onFavoriteClick,
-    showGif,
-    isMobile,
-    showActions,
-    priceText,
-    quantity,
-    onAddItem,
-    onRemoveItem,
-    isEditable,
-    showFavoriteButtons,
-    showSummary
+  meal = {},
+  mealDetailUrl,
+  outOfStock,
+  onMealClick,
+  onChefClick,
+  chef,
+  mainTags = [],
+  onLikeMeal,
+  onUnlikeMeal,
+  isLiked,
+  onFavoriteClick,
+  showGif,
+  isMobile,
+  showActions,
+  priceText,
+  quantity,
+  onAddItem,
+  onRemoveItem,
+  isEditable,
+  showFavoriteButtons,
+  showSummary
 }) => {
+  const clickAndPreventDefault = (e, fn) => {
+    e.stopPropagation && e.stopPropagation()
+    e.preventDefault && e.preventDefault()
+    fn()
+  }
 
-    const clickAndPreventDefault = (e, fn) => {
-        e.stopPropagation && e.stopPropagation()
-        e.preventDefault && e.preventDefault()
-        fn()
-    }
-
-    return  <a
-    href={mealDetailUrl}
-    className={classnames('card-v2', { 'card-v2--sold-out': outOfStock })}
-    onClick={e => clickAndPreventDefault(e, onMealClick)}>
-    {/* <ChefAvatar
+  return (
+    <a
+      href={mealDetailUrl}
+      className={classnames('card-v2', {'card-v2--sold-out': outOfStock})}
+      onClick={e => clickAndPreventDefault(e, onMealClick)}
+    >
+      {/* <ChefAvatar
       image={chef.image}
       fallbackImage={chef.fallbackImage}
       onClick={e => clickAndPreventDefault(e, onChefClick)}
@@ -51,48 +51,49 @@ const CuiRegularMealCard = ({
       className="card-v2__chef"
       size="medium"
     /> */}
-    <div className="card-v2__img-wrapper">
-      <div className="card-v2__tags-wrapper">
-        {mainTags.map(tag => (
-          <CuiTag
-            color={tag.color}
-            fill="solid"
-            size="small"
-            className={classnames('card-v2__tag', {
-              'card-v2__tag-with-icon': !!tag.icon
-            })}>
-            {tag.icon}
-            {tag.label}
-          </CuiTag>
-        ))}
-      </div>
-      {onLikeMeal && onUnlikeMeal && (
-        <CuiMealFavorite
-          checked={isLiked}
-          onClick={e => clickAndPreventDefault(e, onFavoriteClick)}
-          className={classnames('card-v2__favorite', {
-            'card-v2__favorite--empty': !isLiked
-          })}
+      <div className="card-v2__img-wrapper">
+        <div className="card-v2__tags-wrapper">
+          {mainTags.map(tag => (
+            <CuiTag
+              color={tag.color}
+              fill="solid"
+              size="small"
+              className={classnames('card-v2__tag', {
+                'card-v2__tag-with-icon': !!tag.icon
+              })}
+            >
+              {tag.icon}
+              {tag.label}
+            </CuiTag>
+          ))}
+        </div>
+        {onLikeMeal && onUnlikeMeal && (
+          <CuiMealFavorite
+            checked={isLiked}
+            onClick={e => clickAndPreventDefault(e, onFavoriteClick)}
+            className={classnames('card-v2__favorite', {
+              'card-v2__favorite--empty': !isLiked
+            })}
+          />
+        )}
+        <CuiMenuImage
+          primary_image={
+            showGif
+              ? meal.gif || meal.imageUrl || meal.full_path_meal_image
+              : meal.imageUrl || meal.full_path_meal_image
+          }
+          secondary_image={(meal.media && meal.media.secondaryImage) || null}
+          className="card-v2__img"
         />
-      )}
-      <CuiMenuImage
-        primary_image={
-          showGif
-            ? meal.gif || meal.imageUrl || meal.full_path_meal_image
-            : meal.imageUrl || meal.full_path_meal_image
-        }
-        secondary_image={(meal.media && meal.media.secondaryImage) || null}
-        className="card-v2__img"
-      />
-    </div>
-    <div className="card-v2__details">
-      <CuiRegularMealCardTitle
-        title={meal.name}
-        subtitle={meal.short_description}
-        onClick={e => clickAndPreventDefault(e, onMealClick)}
-        size={isMobile ? 'large' : 'medium'}
-      />
-      {/* {showCharacteristics && characteristics_l1.length > 0 && (
+      </div>
+      <div className="card-v2__details">
+        <CuiRegularMealCardTitle
+          title={meal.name}
+          subtitle={meal.short_description}
+          onClick={e => clickAndPreventDefault(e, onMealClick)}
+          size={isMobile ? 'large' : 'medium'}
+        />
+        {/* {showCharacteristics && characteristics_l1.length > 0 && (
         <CharacteristicsL1
           list={characteristics_l1}
           mealId={meal.id}
@@ -102,7 +103,7 @@ const CuiRegularMealCard = ({
       {showCharacteristics && characteristics_l2.length > 0 && (
         <CharacteristicsL2 list={characteristics_l2} size="medium" />
       )} */}
-      {/* {showActions && hasRelatedMeal && (
+        {/* {showActions && hasRelatedMeal && (
         <div onClick={e => clickAndPreventDefault(e, () => {})}>
           <MealCardRelatedMeal
             meal={meal.relatedMeal}
@@ -112,38 +113,39 @@ const CuiRegularMealCard = ({
           />
         </div>
       )} */}
-      {showActions && (
-        <div className="card-v2__actions-container">
-          <div onClick={e => clickAndPreventDefault(e, () => {})}>
-            <CuiMealActions
-              priceText={priceText}
-              quantity={quantity}
-              add={onAddItem}
-              remove={onRemoveItem}
-              isEditable={isEditable}
-              meal={meal}
-            />
+        {showActions && (
+          <div className="card-v2__actions-container">
+            <div onClick={e => clickAndPreventDefault(e, () => {})}>
+              <CuiMealActions
+                priceText={priceText}
+                quantity={quantity}
+                add={onAddItem}
+                remove={onRemoveItem}
+                isEditable={isEditable}
+                meal={meal}
+              />
+            </div>
           </div>
-        </div>
-      )}
-      {outOfStock && <div className="card-v2__sold-out">SOLD OUT</div>}
-      {showFavoriteButtons && onLikeMeal && onUnlikeMeal && (
-        <div className="like_buttons">
-          <CuiMealFavorite
-            className="button-like"
-            onClick={e => clickAndPreventDefault(e, onLikeMeal)}
-          />
-          <button
-            className="button-like"
-            onClick={e => clickAndPreventDefault(e, onUnlikeMeal)}>
-            <img src={Dislike} alt="dislike" />
-          </button>
-        </div>
-      )}
-      {showSummary && <CuiMealSummary quantity={quantity} />}
-    </div>
-  </a>
+        )}
+        {outOfStock && <div className="card-v2__sold-out">SOLD OUT</div>}
+        {showFavoriteButtons && onLikeMeal && onUnlikeMeal && (
+          <div className="like_buttons">
+            <CuiMealFavorite
+              className="button-like"
+              onClick={e => clickAndPreventDefault(e, onLikeMeal)}
+            />
+            <button
+              className="button-like"
+              onClick={e => clickAndPreventDefault(e, onUnlikeMeal)}
+            >
+              <img src={Dislike} alt="dislike" />
+            </button>
+          </div>
+        )}
+        {showSummary && <CuiMealSummary quantity={quantity} />}
+      </div>
+    </a>
+  )
 }
-
 
 export default CuiRegularMealCard
