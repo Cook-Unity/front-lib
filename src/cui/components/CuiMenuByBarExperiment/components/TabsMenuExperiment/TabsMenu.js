4c8f98ae9ff46d2189432af01c7c87c132b82e69
Gabriel Fiocchi <gabriel.fiocchi@cookunity.com>
2023-11-24 18:11:13 -0300

    Fix lint error

--- src/cui/components/CuiMenuByBarExperiment/components/TabsMenuExperiment/TabsMenu.js
+++ src/cui/components/CuiMenuByBarExperiment/components/TabsMenuExperiment/TabsMenu.js
@@ -1,9 +1,9 @@
-import React, { useEffect, useRef, useState } from 'react'
+import React, {useEffect, useRef, useState} from 'react'
 import PropTypes from 'prop-types'
 import classnames from 'classnames'
 
 import './TabsMenu.scss'
-import { fullIconPath } from '../../../../../utils/images'
+import {fullIconPath} from '../../../../../utils/images'
 
 const TabsMenuExperiment = ({
   tabsItems,
