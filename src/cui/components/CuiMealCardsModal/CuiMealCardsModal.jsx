39aab444928fb795f6f4506d6eaadad39cfa0f43
Cristhian Marroquin <cristhian@cookunity.com>
2024-10-02 16:26:39 +0200

    MAR-7729 - Modify crosseling UI

--- src/cui/components/CuiMealCardsModal/CuiMealCardsModal.jsx
+++ src/cui/components/CuiMealCardsModal/CuiMealCardsModal.jsx
@@ -30,7 +30,7 @@ const MealCardsModal = ({
     >
       <div className="cui-mealCardsModal__titles">
         <h2 data-testid="title">{title}</h2>
-        {subtitle && <h2 data-testid="subtitle">{subtitle}</h2>}
+        {subtitle && <h3 data-testid="subtitle">{subtitle}</h3>}
       </div>
       <div className="cui-mealCardsModal__content">
         <div className="cardsContainerOuter">
